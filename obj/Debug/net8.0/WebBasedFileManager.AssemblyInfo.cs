fdf9a028284aabaccb0d4eb530ad802610902b2a
Talfaza <charifyahia4@gmail.com>
2024-08-03 21:04:25 +0100

    add: better navigation

--- obj/Debug/net8.0/WebBasedFileManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebBasedFileManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBasedFileManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c58695389907239c8301dcace0506b99f016ab88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9603d1b2065115ec4a75704021e3621cbdd69c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBasedFileManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBasedFileManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
